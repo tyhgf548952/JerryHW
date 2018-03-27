0cc56fca415356099deea8d8196d88ab68728f92
tyhgf548952 <32482814+tyhgf548952@users.noreply.github.com>
2018-03-27 22:11:08 +0800

    0327
    0327

---
+++ tree/master/0326/0326_calculate.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from source import calculator
+print("請輸入兩個數字.")
+a = int(input())
+b = int(input())
+calculator.calculator(a,b)
+
--- tree/master/0326/source/calculator.py
+++ tree/master/0326/source/calculator.py
@@ -18,25 +18,6 @@ def calculate(operator,num1,num2):
 # In[17]:
 
 
-def calculater(num1,num2):
+def calculator(num1,num2):
     op = input("請輸入加減乘除的符號(+,-,*,/) : ")
     print(calculate(op,num1,num2))
-
-
-# In[19]:
-
-
-calculater(2,3)
-
-
-# In[3]:
-
-
-calculater(2,3,4)
-
-
-# In[13]:
-
-
-calculater(5,6)
-
